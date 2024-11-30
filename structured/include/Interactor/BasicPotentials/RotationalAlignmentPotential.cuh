9d7e35102ae1805e587ac03e4faf629d1e9c0699
pabloibannez <p.ibanez.fre@gmail.com>
2024-11-30 17:58:24 +0100

    Added RotationalAlignmentPotential draft

---
+++ structured/include/Interactor/BasicPotentials/RotationalAlignmentPotential.cuh
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "utils/quaternion.cuh"
+#include "Definitions/Computations.cuh"
+
+#include "Utils/Maths/MatrixOperations.cuh"
+
+#include "Interactor/BasicPotentials/StiffnessMask.cuh"
+
+namespace uammd{
+namespace structured{
+namespace Potentials{
+namespace BasicPotentials{
+namespace RAP{
+
+    // Being A, B rotation matrices, and R some target rotation matrix, the rotational alignment potential is defined as:
+    // U = (real(3) - Frob(A^TB, R))/(real(4))
+
+    static inline __device__ real3 energy(const tensor3& A, const tensor3& B, const tensor3& R){
+        tensor3 C = MatrixOperations::matmul(A.transpose(), B);
+        const real frob = MatrixOperations::frobenius(C, R);
+
+        return (real3(3.0) - frob)/real(4.0);
+    }
+
+    static inline __device__ real3 torque(const tensor3& A, const tensor3& B, const tensor3& R){
+        tensor3 denergydA = -MatrixOperations::matmul(B, R.transpose());
+        return MatrixOperations::Pi(A, denergydA);
+    }
+
+}}}}
---
+++ structured/include/Interactor/BasicPotentials/StiffnessMask.cuh
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "uammd.cuh"
+
+namespace uammd{
+namespace structured{
+namespace Potentials{
+namespace BasicPotentials{
+namespace StiffnessMask{
+
+    // This StiffnessMask is tough to be used in combination with other function f(x) that:
+    // 1. Return values in [0,1]
+    // 2. Has a minimum at x=0
+    //
+    // Mainly StiffnessMask adds an additional parameter, K, that permits to
+    // control the stiffness of the base function.
+
+    static inline __device__ real stiffnessMask(const real& x,
+                                                const real& K){
+        const real kx = K*x;
+
+        return kx/(kx+(real(1.0)-x));
+    }
+
+    static inline __device__ real stiffnessMaskFirstDerivative(const real& x,
+                                                               const real& K){
+        const real kx = K*x;
+        const real denom = kx+(real(1.0)-x);
+
+        return K/(denom*denom);
+    }
+
+    static inline __device__ real stiffnessMaskSecondDerivative(const real& x,
+                                                                const real& K){
+        const real kx = K*x;
+        const real denom = kx+(real(1.0)-x);
+
+        return -real(2.0)*K*(K-real(1.0))/(denom*denom*denom);
+    }
+
+    // Then F(x;K) = stiffnessMask(f(x),K)
+    // F'(x;K) = stiffnessMaskFirstDerivative(f(x),K)*f'(x)
+    // F''(x;K) = stiffnessMaskSecondDerivative(f(x),K)*f'(x)^2 + stiffnessMaskFirstDerivative(f(x),K)*f''(x)
+
+}}}}}
