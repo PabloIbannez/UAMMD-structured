b3d87a03c1cd2a55fa077fb60cbee821641fef05
PabloIbannez <p.ibanez.fre@gmail.com>
2024-12-12 12:42:44 +0100

    Removed old Helix potentials

--- structured/include/Interactor/BasicPotentials/RotationalAlignmentPotential.cuh
+++ structured/include/Interactor/BasicPotentials/RotationalAlignmentPotential.cuh
@@ -16,11 +16,11 @@ namespace RAP{
     // Being A, B rotation matrices, and R some target rotation matrix, the rotational alignment potential is defined as:
     // U = (real(3) - Frob(A^TB, R))/(real(4))
 
-    static inline __device__ real3 energy(const tensor3& A, const tensor3& B, const tensor3& R){
+    static inline __device__ real energy(const tensor3& A, const tensor3& B, const tensor3& R){
         tensor3 C = MatrixOperations::matmul(A.transpose(), B);
         const real frob = MatrixOperations::frobenius(C, R);
 
-        return (real3(3.0) - frob)/real(4.0);
+        return (real(3.0) - frob)/real(4.0);
     }
 
     static inline __device__ real3 torque(const tensor3& A, const tensor3& B, const tensor3& R){
@@ -28,4 +28,4 @@ namespace RAP{
         return MatrixOperations::Pi(A, denergydA);
     }
 
-}}}}
+}}}}}
