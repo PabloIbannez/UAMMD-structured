e31d71a54315458867e77308004a81d921acb1c0
Raul P. Pelaez <raulppelaez@gmail.com>
2025-05-15 10:07:49 +0200

    Add new test

--- tests_pytest/test_imports.py
+++ tests_pytest/test_imports.py
@@ -1,2 +1,9 @@
 def test_import_pyuammd():
     import pyUAMMD
+
+
+def test_has_simulation():
+    # Check pyUAMMD has a simulation module
+    import pyUAMMD
+
+    assert hasattr(pyUAMMD, "simulation")
