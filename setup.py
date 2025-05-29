6670bc36a7468111456ee7c5a578e92426d00c10
Raul P. Pelaez <raulppelaez@gmail.com>
2025-05-29 11:14:57 +0200

    Try empty setup

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-import sys,os
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-setup(
-    packages=find_packages(),
-    url="https://github.com/PabloIbannez/UAMMD-structured",
-)
+setup()
